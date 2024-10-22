7454ce6ac38af8ba50d7ee449ba63f6a1cce9906
CHOON XIANG YEOH - STUDENT <g00425056@atu.ie>
2024-10-22 10:09:27 +0100

    Initial commit

---
+++ src/main/java/ie/atu/week6cicd1/Week6cicd1Application.java
@@ -0,0 +1,13 @@
+package ie.atu.week6cicd1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week6cicd1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week6cicd1Application.class, args);
+	}
+
+}
---
+++ src/test/java/ie/atu/week6cicd1/Week6cicd1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week6cicd1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week6cicd1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
